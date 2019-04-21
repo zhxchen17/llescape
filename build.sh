5c2754d1a9a5d65e926859917984c83bc3eef66f
zhxchen17 <zhxchen17@outlook.com>
2019-04-21 13:21:29 -0400

    update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
-if mkdir build > /dev/null 2>&1 ; then
+if [[ -f "./build/build.ninja" ]]; then
+    cd build
+    ninja
+else
+    mkdir -p build
     cd build
     CC=clang CXX=clang++ cmake -G Ninja ../llvm -DLLVM_USE_SPLIT_DWARF=TRUE -DBUILD_SHARED_LIBS=true -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_ENABLE_PROJECTS='llvm' -DCMAKE_INSTALL_PREFIX=~/llvm -DLLVM_USE_LINKER=gold
-else
-    cd build
 fi
-ninja
